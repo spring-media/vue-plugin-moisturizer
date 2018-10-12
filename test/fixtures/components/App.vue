85a23a45617bc8e10f256949b774423d483ebd2f
Lukas Bombach <lukas.bombach@googlemail.com>
2018-10-12 19:04:44 +0200

    implemented instantiateApp.test.js

---
+++ test/fixtures/components/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>App</div>
+</template>
+
+<script>
+module.exports = {
+  name: "App"
+};
+</script>
+
---
+++ test/instantiateApp.test.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import instantiateApp from "../lib/instantiateApp";
+import App from "./fixtures/components/App.vue";
+
+test("returns a Vue instance without Vue options", () => {
+  expect(instantiateApp(App)).toBeInstanceOf(Vue);
+});
+
+test("returns a Vue instance with Vue options", () => {
+  const vueOptions = true;
+  const vueInstance = instantiateApp(App, { vueOptions });
+  expect(vueInstance.$options.vueOptions).toBe(vueOptions);
+});
+
+test("returns a Vue instance that can be rendered", () => {
+  document.body.innerHTML = '<div id="app"></div>';
+  instantiateApp(App).$mount("#app");
+  expect(document.body.innerHTML).toBe("<div>App</div>");
+});
