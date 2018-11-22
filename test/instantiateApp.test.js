4f5c99f9437b75b244271080c32d20f80145a17f
Lukas Bombach <lukas.bombach@googlemail.com>
2018-11-22 13:18:03 +0100

    fixed tests

--- test/instantiateApp.test.js
+++ test/instantiateApp.test.js
@@ -1,19 +1,21 @@
-import Vue from "vue";
-import {instantiateApp} from "../lib/index";
-import App from "./fixtures/components/App.vue";
+import Vue from 'vue';
+import Moisturizer, { instantiateApp } from '../lib/index';
+import App from './fixtures/components/App.vue';
 
-test("returns a Vue instance without Vue options", () => {
-  expect(instantiateApp(App)).toBeInstanceOf(Vue);
+Vue.use(Moisturizer);
+
+test('returns a Vue instance without Vue options', () => {
+	expect(instantiateApp(App)).toBeInstanceOf(Vue);
 });
 
-test("returns a Vue instance with Vue options", () => {
-  const vueOptions = true;
-  const vueInstance = instantiateApp(App, { vueOptions });
-  expect(vueInstance.$options.vueOptions).toBe(vueOptions);
+test('returns a Vue instance with Vue options', () => {
+	const vueOptions = true;
+	const vueInstance = instantiateApp(App, { vueOptions });
+	expect(vueInstance.$options.vueOptions).toBe(vueOptions);
 });
 
-test("returns a Vue instance that can be rendered", () => {
-  document.body.innerHTML = '<div id="app"></div>';
-  instantiateApp(App).$mount("#app");
-  expect(document.body.innerHTML).toBe("<div>App</div>");
+test('returns a Vue instance that can be rendered', () => {
+	document.body.innerHTML = '<div id="app"></div>';
+	instantiateApp(App).$mount('#app');
+	expect(document.body.innerHTML).toBe('<div>App</div>');
 });
