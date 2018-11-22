5db5515329e21ec08bc5c40917181d826a68fb26
Lukas Bombach <lukas.bombach@googlemail.com>
2018-11-22 12:38:21 +0100

    made slots not require to be passed Vue to

--- test/utils.js
+++ test/utils.js
@@ -5,28 +5,30 @@ import { createLocalVue, mount } from '@vue/test-utils';
 import MoisturizerVuePlugin from '../lib/moisturizerVuePlugin';
 
 export const createHTMLNode = (name, component, innerHtml = '', attributes = {}) => {
-  const fp = `fingerprint="${new Fingerprint (component).fingerprint()}"`;
-  const props = `props="${new Props(component).serialize(component.$props || [])}"`;
-  const slots = `slots="${new Slots(component).serialize(component.$slots || [])}"`;
-  const attrs = Object.keys(attributes).map(x => `${x}="${attributes[x]}"`);
+	const fp = `fingerprint="${new Fingerprint(component).fingerprint()}"`;
+	const props = `props="${new Props(component).serialize(component.$props || [])}"`;
+	const slots = `slots="${new Slots(component).serialize(component.$slots || [])}"`;
+	const attrs = Object.keys(attributes).map(x => `${x}="${attributes[x]}"`);
 
-  return `<${name} ` +
-    [fp, props, slots].map(x => 'data-hydrate-' + x).join(' ')
-    + (!attrs ? '' : ' ' + attrs.join(' '))
-    + `>${innerHtml}</${name}>`;
+	return (
+		`<${name} ` +
+		[fp, props, slots].map(x => 'data-hydrate-' + x).join(' ') +
+		(!attrs ? '' : ' ' + attrs.join(' ')) +
+		`>${innerHtml}</${name}>`
+	);
 };
 
 export const setLocalVueEnv = (localVue, isServer) => {
-  Object.defineProperty (localVue.prototype, '$isServer', {
-    get: () => isServer,
-  });
+	Object.defineProperty(localVue.prototype, '$isServer', {
+		get: () => isServer,
+	});
 };
 
 export const init = (components = []) => {
-  const localVue = createLocalVue ();
-  const isServer = true;
-  setLocalVueEnv (localVue, isServer);
-  localVue.use (MoisturizerVuePlugin);
-  const mounted = components.map (c => mount (c));
-  return { mounted, localVue };
+	const localVue = createLocalVue();
+	const isServer = true;
+	setLocalVueEnv(localVue, isServer);
+	localVue.use(MoisturizerVuePlugin);
+	const mounted = components.map(c => mount(c));
+	return { mounted, localVue };
 };
